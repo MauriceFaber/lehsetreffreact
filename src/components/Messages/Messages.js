c478d52000f13ca4b1d97b8bfd52f6bd8e2da5e2
MauriceFaber <67681325+MauriceFaber@users.noreply.github.com>
2022-03-24 11:47:08 +0100

    nachrichten schreiben in jedem thread möglich

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -8,13 +8,23 @@ import { useAuth } from "../../contexts/auth";
 
 export default function Messages() {
   const { threadName } = useParams();
+  const [thread, setThread] = useState(undefined);
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState("");
   const { user, authenticated } = useAuth();
 
   useEffect(async () => {
+    await loadThread();
     await loadMessages();
   }, []);
+
+  async function loadThread() {
+    let request = new Request(domain + "/threads?threadName=" + threadName);
+    let data = await fetch(request);
+    data = await data.json();
+    document.title = threadName;
+    setThread(data);
+  }
 
   async function loadMessages() {
     let request = new Request(domain + "/messages?threadName=" + threadName);
@@ -24,17 +34,13 @@ export default function Messages() {
     setMessages(data);
   }
 
-  if (messages.length == 0) {
-    return <div>Keine Nachrichten vorhanden.</div>;
-  }
-
   async function sendText() {
     let request = new Request(domain + "/messages", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
       },
-      body: `apiKey=${user.apiKey}&content=${text}&contentType=0&threadId=${1}`,
+      body: `apiKey=${user.apiKey}&content=${text}&contentType=0&threadId=${thread.id}`,
     });
     let result = await fetch(request);
     if (result.ok) {
@@ -51,15 +57,19 @@ export default function Messages() {
     }
   }
 
+  if (!thread) {
+    return <div>...</div>;
+  }
+
   return (
     <div className="message-page-content">
-      <h2>{messages.length > 0 ? messages[0].thread.caption : ""}</h2>
+      <h2>{thread.caption}</h2>
       <br></br>
+      {messages.length == 0 ? <div>Keine Nachrichten vorhanden.</div> : null}
       {messages.map((message, index) => {
         const key = `message_${message.id}`;
         return <Message key={key} message={message} />;
       })}
-
       {authenticated ? (
         <div className="send-message-box">
           <div className="button-wrapper first-button">
